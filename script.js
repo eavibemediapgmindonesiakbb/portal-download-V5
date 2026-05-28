39897a5c3241e5a7c70053212bbb87e46d3d13eb
eavibemediapgmindonesiakbb <pgmindonesiakbb23@gmail.com>
2026-05-28 15:20:46 +0700

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,42 @@ window.onload = () => {
 }
 
 // Sisanya sama kayak kode sebelumnya...
+function handleLogin() {
+  const nik = document.getElementById('nik').value;
+  const pass = document.getElementById('password').value;
+  
+  if(!nik || !pass) {
+    alert('NIK dan Password wajib diisi');
+    return;
+  }
+
+  document.querySelector('.btn-primary').innerHTML = '<i class="fa-solid fa-spinner fa-spin"></i> Loading...';
+
+  google.script.run.withSuccessHandler(res => {
+    if(res.success) {
+      sessionStorage.setItem('nik', res.nik);
+      if(res.role === 'admin') {
+        alert('Dashboard Admin belum dibuat di versi ini');
+        document.querySelector('.btn-primary').innerHTML = '<i class="fa-solid fa-right-to-bracket"></i> Masuk';
+      } else {
+        // Ini yang bener buat Apps Script
+        google.script.run.withSuccessHandler(html => {
+          document.open();
+          document.write(html);
+          document.close();
+        }).getDashboardPage();
+      }
+    } else {
+      alert(res.message);
+      document.querySelector('.btn-primary').innerHTML = '<i class="fa-solid fa-right-to-bracket"></i> Masuk';
+    }
+  }).withFailureHandler(err => {
+    alert('Error: ' + err.message);
+    document.querySelector('.btn-primary').innerHTML = '<i class="fa-solid fa-right-to-bracket"></i> Masuk';
+  }).loginUser(nik, pass);
+}
+
+// Tambah fungsi ini di Code.gs
+function getDashboardPage() {
+  return HtmlService.createTemplateFromFile('dashboard').evaluate().getContent();
+}
